c9948ff4bcc27aecb4a1f14870a328bbd4d72e75
passcod <none@none>
2010-06-05 18:02:46 +1200

    local storage override

--- thebook/refresh.php
+++ thebook/refresh.php
@@ -3,7 +3,8 @@ include_once("markdown.php");
 
 function getIt($file) {
 	$template = file_get_contents('template');
-	$raw = file_get_contents('http://beta.intuxication.org/passcod/book/raw-file/tip/'.$file.'.md');
+	//$raw = file_get_contents('http://beta.intuxication.org/passcod/book/raw-file/tip/'.$file.'.md');
+	$raw = file_get_contents('/home/felix/Documents/thebook/'.$file.'.md'); //local override
 	$raw = Markdown($raw);
 	$raw = str_replace( 'page_content_goes_here', $raw, $template );
 	return $raw;
